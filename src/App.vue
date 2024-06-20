aed8d7ca18b1391d8bcbdf7fa80c452f5fd4d19b
Keriz0892 <77613724+RSVNzxzxbxe@users.noreply.github.com>
2024-06-20 23:32:27 +0700

    sa

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bg-[#34495e] border-2 border-[#485563] border-solid p-8 m-auto my-40 w-80 rounded-lg">
-        <h4 class="text-white text-lg mb-4">CEK KODAM</h4>
+        <h4 class="text-white text-lg mb-4 m-auto">CEK KODAM</h4>
         <input v-model="inputNama" type="text" placeholder="Masukkan Nama"
             class="w-full p-2 mb-4 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500">
         <button @click="cekKodam"
             class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
-        <p v-if="hasilKodam" class="text-white mt-4">Kodam {{ inputNama }} {{ hasilKodam }}</p>
+        <p v-if="hasilKodam" class="text-white mt-4">Kodam {{ inputNama }} adalah {{ hasilKodam }}</p>
     </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
     setup() {
         const inputNama = ref('');
         const hasilKodam = ref(null);
-        const kodamList = ['Gayung Pecah', 'Pace', 'Tukul Arwana', 'Setang Motor', 'Letkol BIBD', 'Genderuwo Pargoy', 'Kenalpot Bolong', 'Kipas Muter'];
+        const kodamList = ['Gayung Pecah', 'Pace', 'Tukul Arwana', 'Setang Motor', 'Letkol BIBD', 'Genderuwo Pargoy', 'Kenalpot Bolong', 'Kipas Muter', 'Macan Madura', 'Sate Madura', 'Angsa Kungfu'];
 
         const getRandomKodam = () => {
             let randomIndex = Math.floor(Math.random() * kodamList.length);
@@ -26,7 +26,7 @@ export default {
         const cekKodam = () => {
             if (inputNama.value.trim() !== '') {
                 hasilKodam.value = getRandomKodam();
-                speakResult(`Kodam ${inputNama.value} ${hasilKodam.value}`); // Speak the result
+                speakResult(`Kodam ${inputNama.value} adalah ${hasilKodam.value}`); // Speak the result
                 setTimeout(() => {
                     hasilKodam.value = null;
                 }, 4000);
